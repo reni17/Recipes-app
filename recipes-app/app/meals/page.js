44f0e3f18351bad520a286609b18a9e3eda9c807
reni17 <69053305+reni17@users.noreply.github.com>
2024-01-16 14:42:04 +0200

    Add project setup and header

---
+++ recipes-app/app/meals/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Meals() {
+  return <div>All meals!</div>;
+}
+
+export default Meals;
---
+++ recipes-app/components/main-header-background.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MainHeaderBackground() {
+  return <div></div>;
+}
+
+export default MainHeaderBackground;
